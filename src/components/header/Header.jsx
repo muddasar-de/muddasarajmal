bedc82671ed4e6c53b6fcb66a976c4871876d65d
muddasar-de <muddasarajmal87@gmail.com>
2022-04-19 12:44:58 +0500

    Typography Issues

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,11 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { useEffect } from 'react';
 import './header.css';
 import CTA from './CTA';
 import Me from '../../assests/me.png';
 import HeaderSocial from './HeaderSocial';
-import { BsArrowRight } from 'react-icons/bs';
+import { BsArrowRight, BsFillPenFill } from 'react-icons/bs';
+
 import { init } from 'ityped';
 const Header = () => {
   const textRef = useRef();
@@ -19,7 +20,10 @@ const Header = () => {
       <div className='container header__container'>
         <h5>Hello, I'm</h5>
         <h1>Muddasar Ajmal</h1>
-        <h5 className='text-light' ref={textRef}></h5>
+        <h5 className='text-light'>
+          <span ref={textRef}></span>
+          <BsFillPenFill />
+        </h5>
         <CTA />
         <HeaderSocial />
         <div className='me'>
